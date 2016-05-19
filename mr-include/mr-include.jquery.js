861931166c6d6cf76dfc2ec62167ffcce7323274
Виталий Кулешин <antivitla@gmail.com>
2016-05-19 13:50:33 +0300

    fix root file includes

--- mr-include/mr-include.jquery.js
+++ mr-include/mr-include.jquery.js
@@ -2,14 +2,15 @@
 	function init ($item) {
 		$item.data("mr-include", jQuery.extend($item.data("mr-include") || {}, { includes: 0 })); // track nested includes
 
-		var selector = "mr-include[src]";
+		var selector = "mr-include[src]",
+			$parent = ($item.get(0).tagName == "MR-INCLUDE" ? $item.data("mr-include").parent : $item);
+
 		$item.find(selector).addBack(selector).each(function () {
 			var placeholder = $(this),
 				url = placeholder.attr("src");
 
 			$item.data("mr-include").includes += 1; // track nested includes
-
-			include(placeholder).src(url, $item);
+			include(placeholder).src(url, $parent);
 		});
 
 		// track nested includes
@@ -26,7 +27,9 @@
 					var template = $(html).filter("*");
 					$placeholder.replaceWith(template);
 					template.each(function () {
-						$(this).data("mr-include", jQuery.extend($(this).data("mr-include") || {}, { parent: $parent })); // track nested includes
+						$(this).data("mr-include", jQuery.extend($(this).data("mr-include") || {}, {
+							parent: $parent
+						})); // track nested includes
 						$(this).trigger(jQuery.Event("mr-include"));
 					});
 				});
