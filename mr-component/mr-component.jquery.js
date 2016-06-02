47ac3512c38a301b14f0ebdf68ca6ba567c133f2
Виталий Кулешин <antivitla@gmail.com>
2016-06-02 18:53:23 +0300

    Fix completion event of documentready-existing element with includes

--- mr-component/mr-component.jquery.js
+++ mr-component/mr-component.jquery.js
@@ -4,37 +4,37 @@
 
 var mr = mr || {
 
-	/*
+  /*
 
-	Сложившаяся система инициализации "компонентов" - с учётом иклюда *.html файлов
-	в реальном времени. То есть инициализация проводится и когда "DOM ready" и когда
-	инклюдится некий *.html.
+  Сложившаяся система инициализации "компонентов" - с учётом иклюда *.html файлов
+  в реальном времени. То есть инициализация проводится и когда "DOM ready" и когда
+  инклюдится некий *.html.
 
-	Инклюд <mr-include> генерит событие "include", которое и ловит данный инициализатор.
+  Инклюд <mr-include> генерит событие "include", которое и ловит данный инициализатор.
 
-	*/
-	component: function (selector) {
-		var mr = this;
-		return {
-			init: function (callback) {
-				$(document).on("ready", function () {
-					$(document).find(selector).each(function () {
-						if (!$(this).get(0).mrComponentReady) {
-							callback($(this));
-							$(this).get(0).mrComponentReady = true;
-						}
-					});
-				});
+  */
+  component: function (selector) {
+    var mr = this
+    return {
+      init: function (callback) {
+        $(document).on('ready', function () {
+          $(document).find(selector).each(function () {
+            if (!$(this).get(0).mrComponentReady && $(this).find('mr-include').length < 1) {
+              callback($(this))
+              $(this).get(0).mrComponentReady = true
+            }
+          })
+        })
 
-				$(document).on("mr-include-complete", function (event) {
-					$(event.target).find(selector).addBack(selector).each(function () {
-						if (!$(this).get(0).mrComponentReady) {
-							callback($(this));
-							$(this).get(0).mrComponentReady = true;
-						}
-					});
-				});
-			}
-		};
-	}
-};
+        $(document).on('mr-include-complete', function (event) {
+          $(event.target).find(selector).addBack(selector).each(function () {
+            if (!$(this).get(0).mrComponentReady) {
+              callback($(this))
+              $(this).get(0).mrComponentReady = true
+            }
+          })
+        })
+      }
+    }
+  }
+}
